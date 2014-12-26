8198393502f864fa23bf0ad7db5a5f98320c92a0
taukir4u <taukir4u@gmail.com>
2014-12-26 20:43:40 +0600

    C#
    Signed-off-by: taukir4u <taukir4u@gmail.com>

---
+++ Salary/SalaryAppUI.Designer.cs
@@ -0,0 +1,106 @@
+﻿namespace Salary
+{
+    partial class SalaryAppUI
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.CalculateButton = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.employeeTextBox = new System.Windows.Forms.TextBox();
+            this.totalSalaryTextBox = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // CalculateButton
+            // 
+            this.CalculateButton.Location = new System.Drawing.Point(110, 169);
+            this.CalculateButton.Name = "CalculateButton";
+            this.CalculateButton.Size = new System.Drawing.Size(79, 23);
+            this.CalculateButton.TabIndex = 0;
+            this.CalculateButton.Text = "Calculate";
+            this.CalculateButton.UseVisualStyleBackColor = true;
+            this.CalculateButton.Click += new System.EventHandler(this.CalculateButton_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(8, 40);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(85, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Emlpoyee Salary";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(30, 92);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(63, 13);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Total Salary";
+            // 
+            // employeeTextBox
+            // 
+            this.employeeTextBox.Location = new System.Drawing.Point(123, 38);
+            this.employeeTextBox.Name = "employeeTextBox";
+            this.employeeTextBox.Size = new System.Drawing.Size(135, 20);
+            this.employeeTextBox.TabIndex = 4;
+            // 
+            // totalSalaryTextBox
+            // 
+            this.totalSalaryTextBox.Location = new System.Drawing.Point(123, 89);
+            this.totalSalaryTextBox.Name = "totalSalaryTextBox";
+            this.totalSalaryTextBox.Size = new System.Drawing.Size(135, 20);
+            this.totalSalaryTextBox.TabIndex = 5;
+            // 
+            // SalaryAppUI
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(344, 309);
+            this.Controls.Add(this.totalSalaryTextBox);
+            this.Controls.Add(this.employeeTextBox);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.CalculateButton);
+            this.Name = "SalaryAppUI";
+            this.Text = "SalaryApp";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button CalculateButton;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox employeeTextBox;
+        private System.Windows.Forms.TextBox totalSalaryTextBox;
+    }
+}
+
---
+++ Salary/SalaryAppUI.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Salary
+{
+    public partial class SalaryAppUI : Form
+    {
+        public SalaryAppUI()
+        {
+            InitializeComponent();
+        }
+
+        private int salary;
+        private double totalsalary;
+
+        private void CalculateButton_Click(object sender, EventArgs e)
+        {
+            salary = Convert.ToInt32(employeeTextBox.Text);
+            {
+               if (salary > 10000)
+                    totalSalaryTextBox.Text = ((salary*100/5).ToString());
+
+                else if (salary >= 8000 && salary <= 10000)
+                    totalSalaryTextBox.Text = ((salary*100/6).ToString());
+
+                else
+                    totalSalaryTextBox.Text = ((salary*100/7).ToString());
+            }
+        }
+    }
+}
